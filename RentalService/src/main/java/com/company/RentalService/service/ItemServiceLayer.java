249a8cf790cd1cb9a48fd06baba7f5924e0d485b
Remy <remington49@rocketmail.com>
2019-09-12 11:08:20 -0400

    Adding Item service Layer class file

---
+++ RentalService/src/main/java/com/company/RentalService/service/ItemServiceLayer.java
@@ -0,0 +1,11 @@
+package com.company.RentalService.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemServiceLayer {
+
+    //private ItemDao itemDao;
+
+
+}
